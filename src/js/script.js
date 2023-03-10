74bde1f7169a26eeab52c0380afe6a352475cc54
Copy-Kat <megumin2301@gmail.com>
2023-03-10 04:58:56 +0100

    all formatting, need clean up

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,8 @@ const keyBinds = {
     bold : "b",
     italics : "i",
     underline : "u",
-    undo : "z"
+    undo : "z",
+    test : "t"
     
 }
 
@@ -72,19 +73,22 @@ document.addEventListener("keydown", (event) => {
 
     if (event.ctrlKey) {
         //event.preventDefault();
+        const selection = window.getSelection().getRangeAt(0)       
+
         switch (event.key) {
             case keyBinds.bold:
                 event.preventDefault();
-                format("bold");
+                format("bold", selection);
                 break;
             case keyBinds.italics:
                 event.preventDefault();
-                format("em");
+                format("em", selection);
                 break;
             case keyBinds.underline:
                 event.preventDefault();
-                format("u");
+                format("u", selection);
                 break;
+            
             case keyBinds.undo:
                 event.preventDefault();
                 currentState--;
@@ -120,98 +124,200 @@ document.addEventListener("keyup", () => {
 
 
 // TEXT FORMATING
-function format(elementType) {
+function format(elementType, selection) {
     // get the text string of the editor
-    var text, text1, textArea; 
-    var multi_line = false;
+    var text, text1; 
+    var multi_para = false;
     var undo = false;
 
     // get selection
-    const selection = window.getSelection().getRangeAt(0);
+    // const selection = window.getSelection().getRangeAt(0);
 
-    // get containers
+    // // get containers
     const startContainer = selection.startContainer;
     const endContainer = selection.endContainer;
-
-    //console.log(startContainer.parentElement.classList , endContainer.parentElement.classNames)
-
-    // classes 
+    
     const startContainerClasses = startContainer.parentElement.classList
     const endContainerClasses = endContainer.parentElement.classList
+    
+    var startOffset = selection.startOffset;
+    var endOffset = selection.endOffset;
 
     if (startContainerClasses.contains(elementType) && endContainerClasses.contains(elementType)) {
         undo = true;
         console.log(undo);
     }
-
-    // console.log(startContainerClasses, endContainerClasses)
+    //console.log(startContainer.parentElement.classList , endContainer.parentElement.classNames)
 
     if (startContainer === endContainer) {
-        textArea = startContainer.parentElement.parentElement
+        var textArea = startContainer.parentElement.parentElement
+    }
+    else if (selection.commonAncestorContainer.tagName == "P") {
+        var textArea = selection.commonAncestorContainer
     }
     else {
-        textArea = selection.commonAncestorContainer
-    }
-    
-    // get the main editor
-    //var textArea = startContainer.parentElement;
-    //document.getElementById(id).focus(); // keep focus
-
-    //console.log(startContainer, endContainer);
-
-    // get childnodes
-    var childnodes =  textArea.childNodes;
-
-    // console.log(childnodes)
-    
-    // get the indecies 
-    var indexStart, indexEnd
-
-    // assumption: if not span then depth 1
-    var indexStart = Array.from(childnodes).indexOf(startContainer.parentElement);
-
-    var indexEnd = Array.from(childnodes).indexOf(endContainer.parentElement);
-
-    // console.log(indexStart, indexEnd)
-
-    // get offset
-    var startOffset = selection.startOffset;
-    var endOffset = selection.endOffset;
-
-    // console.log(startOffset, endOffset)
-
-    // get the cases
-
-    // case 1: single line no boling
-    if (indexStart === indexEnd) {
-        // console.log("1")
-        // get text
-        text = startContainer.parentElement.innerText;
-
-        const newSpan = document.createElement("span");
-        const formatedSpan = document.createElement("span");
-
-        newSpan.innerHTML = text.slice(0, startOffset).replaceAll(" ", "&nbsp;");
-        formatedSpan.innerHTML = text.slice(startOffset, endOffset).replaceAll(" ", "&nbsp;");
-
-        textArea.insertBefore(newSpan, startContainer.parentElement);
-        textArea.insertBefore(formatedSpan, startContainer.parentElement);
-
-        if (startContainerClasses.length != 0) {
-            formatedSpan.classList = startContainerClasses  
-            newSpan.classList = startContainerClasses  
-        }
-
-        if (undo) {
-            formatedSpan.classList.remove(elementType);
-        } else {
-            formatedSpan.classList.add(elementType);
-        }
-
-        startContainer.parentElement.innerHTML = text.slice(endOffset).replaceAll(" ", "&nbsp;");
+        multi_para = true
     }
 
-    else if (indexStart + 1 === indexEnd) {
+    if (!multi_para) {
+
+        // classes 
+        
+
+        
+        //console.log(textArea)
+
+        
+
+        // console.log(startContainerClasses, endContainerClasses)
+        
+        // get the main editor
+        //var textArea = startContainer.parentElement;
+        //document.getElementById(id).focus(); // keep focus
+
+        //console.log(startContainer, endContainer);
+
+        // get childnodes
+        var childnodes =  textArea.childNodes;
+
+        // console.log(childnodes)
+
+        // assumption: if not span then depth 1
+        var indexStart = Array.from(childnodes).indexOf(startContainer.parentElement);
+
+        var indexEnd = Array.from(childnodes).indexOf(endContainer.parentElement);
+
+        // console.log(indexStart, indexEnd)
+
+        // get offset
+        
+
+        // console.log(startOffset, endOffset)
+
+        // get the cases
+
+        // case 1: single line no boling
+        if (indexStart === indexEnd) {
+            // console.log("1")
+            // get text
+            text = startContainer.parentElement.innerText;
+
+            const newSpan = document.createElement("span");
+            const formatedSpan = document.createElement("span");
+
+            newSpan.innerHTML = text.slice(0, startOffset).replaceAll(" ", "&nbsp;");
+            formatedSpan.innerHTML = text.slice(startOffset, endOffset).replaceAll(" ", "&nbsp;");
+
+            textArea.insertBefore(newSpan, startContainer.parentElement);
+            textArea.insertBefore(formatedSpan, startContainer.parentElement);
+
+            if (startContainerClasses.length != 0) {
+                formatedSpan.classList = startContainerClasses  
+                newSpan.classList = startContainerClasses  
+            }
+
+            if (undo) {
+                formatedSpan.classList.remove(elementType);
+            } else {
+                formatedSpan.classList.add(elementType);
+            }
+
+            startContainer.parentElement.innerHTML = text.slice(endOffset).replaceAll(" ", "&nbsp;");
+        }
+
+        else if (indexStart + 1 === indexEnd) {
+
+            text = startContainer.parentElement.innerText;
+            text1 = endContainer.parentElement.innerText;
+
+            const formatedspan1 = document.createElement("span");
+            const formatedspan2 = document.createElement("span");
+
+            formatedspan1.innerHTML = text.slice(startOffset).replaceAll(" ", "&nbsp;") 
+            formatedspan2.innerHTML =  text1.slice(0, endOffset).replaceAll(" ", "&nbsp;");
+
+            if (startContainerClasses.length != 0) {
+                formatedspan1.classList = startContainerClasses    
+            }
+            if (endContainerClasses.length != 0) {
+                formatedspan2.classList = endContainerClasses    
+            }
+
+            //console.log(startContainerClasses, endContainerClasses)
+
+            if (undo) {
+                formatedspan1.classList.remove(elementType);
+                formatedspan2.classList.remove(elementType)
+            } else {
+                formatedspan1.classList.add(elementType);
+                formatedspan2.classList.add(elementType);
+            }
+
+            textArea.insertBefore(formatedspan1, endContainer.parentElement);
+            textArea.insertBefore(formatedspan2, endContainer.parentElement);
+            
+            startContainer.parentElement.innerHTML = text.slice(0, startOffset).replaceAll(" ", "&nbsp;");
+            endContainer.parentElement.innerHTML = text1.slice(endOffset).replaceAll(" ", "&nbsp;");
+
+        }
+
+        else {
+
+            text = startContainer.parentElement.innerText;
+            text1 = endContainer.parentElement.innerText;
+            
+            for (let index = indexStart + 1; index < indexEnd; index++) {
+                if (undo && childnodes[index].classList.contains(elementType)) {
+                    childnodes[index].classList.remove(elementType);
+                } else { childnodes[index].classList.add(elementType); }  
+            }
+
+            const formatedspan1 = document.createElement("span");
+            const formatedspan2 = document.createElement("span");
+
+            formatedspan1.innerHTML = text.slice(startOffset).replaceAll(" ", "&nbsp;") 
+            formatedspan2.innerHTML =  text1.slice(0, endOffset).replaceAll(" ", "&nbsp;");
+
+            if (startContainerClasses.length != 0) {
+                formatedspan1.classList = startContainerClasses    
+            }
+            if (endContainerClasses.length != 0) {
+                formatedspan2.classList = endContainerClasses    
+            }
+
+            if (undo) {
+                formatedspan1.classList.remove(elementType);
+                formatedspan2.classList.remove(elementType)
+            } else {
+                formatedspan1.classList.add(elementType);
+                formatedspan2.classList.add(elementType);
+            }
+
+            textArea.insertBefore(formatedspan1, childnodes[indexStart+1]);
+            textArea.insertBefore(formatedspan2, endContainer.parentElement);
+            
+            startContainer.parentElement.innerHTML = text.slice(0, startOffset).replaceAll(" ", "&nbsp;");
+            endContainer.parentElement.innerHTML = text1.slice(endOffset).replaceAll(" ", "&nbsp;");
+
+        }
+    } else {
+        const editorChilds = document.querySelectorAll("div.main>p")
+
+        console.log(startOffset, endOffset)
+
+        var parent1 = startContainer.parentElement.parentElement
+        var parent2 = endContainer.parentElement.parentElement
+
+        var indexParentStart = Array.from(editorChilds).indexOf(parent1);
+
+        var indexParentEnd = Array.from(editorChilds).indexOf(parent2);
+
+        var childnodesStart =  parent1.childNodes;
+        var childnodesEnd =  parent2.childNodes;
+
+        var indexStart = Array.from(childnodesStart).indexOf(startContainer.parentElement);
+
+        var indexEnd = Array.from(childnodesEnd).indexOf(endContainer.parentElement);
 
         text = startContainer.parentElement.innerText;
         text1 = endContainer.parentElement.innerText;
@@ -219,17 +325,12 @@ function format(elementType) {
         const formatedspan1 = document.createElement("span");
         const formatedspan2 = document.createElement("span");
 
-        formatedspan1.innerHTML = text.slice(startOffset).replaceAll(" ", "&nbsp;") 
-        formatedspan2.innerHTML =  text1.slice(0, endOffset).replaceAll(" ", "&nbsp;");
-
         if (startContainerClasses.length != 0) {
             formatedspan1.classList = startContainerClasses    
         }
         if (endContainerClasses.length != 0) {
             formatedspan2.classList = endContainerClasses    
         }
-
-        console.log(startContainerClasses, endContainerClasses)
 
         if (undo) {
             formatedspan1.classList.remove(elementType);
@@ -239,61 +340,48 @@ function format(elementType) {
             formatedspan2.classList.add(elementType);
         }
 
-        textArea.insertBefore(formatedspan1, endContainer.parentElement);
-        textArea.insertBefore(formatedspan2, endContainer.parentElement);
-        
-        startContainer.parentElement.innerHTML = text.slice(0, startOffset).replaceAll(" ", "&nbsp;");
-        endContainer.parentElement.innerHTML = text1.slice(endOffset).replaceAll(" ", "&nbsp;");
-
-    }
-
-    else {
-
-        text = startContainer.parentElement.innerText;
-        text1 = endContainer.parentElement.innerText;
-        
-        for (let index = indexStart + 1; index < indexEnd; index++) {
-            childnodes[index].classList.add(elementType)
-        }
-
-        const formatedspan1 = document.createElement("span");
-        const formatedspan2 = document.createElement("span");
-
         formatedspan1.innerHTML = text.slice(startOffset).replaceAll(" ", "&nbsp;") 
         formatedspan2.innerHTML =  text1.slice(0, endOffset).replaceAll(" ", "&nbsp;");
 
-        if (startContainerClasses.length != 0) {
-            formatedspan1.classList = startContainerClasses    
-        }
-        if (endContainerClasses.length != 0) {
-            formatedspan2.classList = endContainerClasses    
+        for (let index = indexStart + 1; index < childnodesStart.length; index++) {
+            if (undo) { childnodesStart[index].classList.remove(elementType) } else { childnodesStart[index].classList.add(elementType) }
         }
 
-        console.log(startContainerClasses, endContainerClasses)
-
-        if (undo) {
-            formatedspan1.classList.remove(elementType);
-            formatedspan2.classList.remove(elementType)
-        } else {
-            formatedspan1.classList.add(elementType);
-            formatedspan2.classList.add(elementType);
+        for (let index = 0; index < indexEnd; index++) {
+            if (undo) { childnodesEnd[index].classList.remove(elementType) } else { childnodesEnd[index].classList.add(elementType) }
         }
 
-        textArea.insertBefore(formatedspan1, childnodes[indexStart+1]);
-        textArea.insertBefore(formatedspan2, endContainer.parentElement);
-        
+        parent1.insertBefore(formatedspan1, childnodesStart[indexStart+1]);
+        parent2.insertBefore(formatedspan2, endContainer.parentElement);
+
         startContainer.parentElement.innerHTML = text.slice(0, startOffset).replaceAll(" ", "&nbsp;");
         endContainer.parentElement.innerHTML = text1.slice(endOffset).replaceAll(" ", "&nbsp;");
 
+        for (let index = indexParentStart + 1; index <= indexParentEnd - 1; index++) {
+            editorChilds[index].childNodes.forEach(element => {
+                if (undo) {element.classList.remove(elementType)} else {element.classList.add(elementType)};
+            });
+        }
+
+        console.log(indexStart, indexEnd, childnodesStart, childnodesEnd)
     }
+
+    
 
     // Clean up
-    var newChildNodes = textArea.childNodes;
-    newChildNodes.forEach(element => {
-        if (element.innerText === "") {
-            element.remove();
-        }
+    document.getElementById("main").childNodes.forEach(child => {
+        let newChildNodes = child.childNodes;
+        newChildNodes.forEach(element => {
+            if (element.innerText === "") {
+                element.remove();
+            }
+        });
     });
+
+    //console.log(document.getElementById("main").childNodes)
+    
+
+    
 }
 
 // TODO: FIX THIS
